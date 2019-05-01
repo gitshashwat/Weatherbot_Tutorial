08c15da2a1d52027600a489d0dcfb2a610f52927
Shashwat Kumar <forwardtoshashwat@gmail.com>
2019-05-01 20:40:20 +0530

    Updated train_init.py
    Made a few changes as per the common issues including exceptions occuring in passing policy configuration parameters.  This is running well.

--- Full Code/train_init.py
+++ Full Code/train_init.py
@@ -7,6 +7,7 @@ import logging
 from rasa_core.agent import Agent
 from rasa_core.policies.keras_policy import KerasPolicy
 from rasa_core.policies.memoization import MemoizationPolicy
+from rasa_core.featurizers import (MaxHistoryTrackerFeaturizer, BinarySingleStateFeaturizer)
 
 if __name__ == '__main__':
 	logging.basicConfig(level='INFO')
@@ -14,14 +15,15 @@ if __name__ == '__main__':
 	training_data_file = './data/stories.md'
 	model_path = './models/dialogue'
 	
-	agent = Agent('weather_domain.yml', policies = [MemoizationPolicy(), KerasPolicy()])
+	featurizer = MaxHistoryTrackerFeaturizer(BinarySingleStateFeaturizer(), max_history=5)
+	agent = Agent('weather_domain.yml',
+              policies=[MemoizationPolicy(max_history=5),
+                        KerasPolicy(featurizer,epochs = 500,batch_size = 10)])
+	data = agent.load_data(training_data_file)
 	
 	agent.train(
-			training_data_file,
+			data,
 			augmentation_factor = 50,
-			max_history = 2,
-			epochs = 500,
-			batch_size = 10,
 			validation_split = 0.2)
 			
 	agent.persist(model_path)
